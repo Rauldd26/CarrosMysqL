cf804612ec9caa6308d6f3c220c2e1b031170833
António Lira Fernandes <alf@esmonserrate.org>
2024-11-15 11:32:52 +0000

    first commit

---
+++ .history/config_20241115105324.php
@@ -0,0 +1,16 @@
+<?php 
+
+define("_URL", 'https://mydomais.org/');
+
+define("_SERVER", 'localhost');
+define("_BDUSER", 'turma12r');
+define("_BDPASS", 'ASaQakcXbf9Wh70');
+define("_BD", 'turma12r');
+
+define("_JSON_CARROS", '../json/dados.json');
+
+define("_CAMINHO_TEMPLATE", '../templates/base/');
+define("_CAMINHO_ADM", '../admin/');
+define("_CAMINHO_ERROS", "../templates/Errors/");
+
+?>
